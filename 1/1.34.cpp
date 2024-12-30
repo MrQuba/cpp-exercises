39b724798363a0f7f5279a43e0b6cece36a34a47
MrQuba <mrquba4@gmail.com>
2024-12-30 22:19:21 +0100

    1.34 improvements

--- 1/1.34.cpp
+++ 1/1.34.cpp
@@ -1,19 +1,13 @@
+#include <cctype>
 #include <iostream>
-#include <type_traits>
-template <typename  A>
-requires std::is_arithmetic_v<A>
-bool in_range(const A n, const A lb, const A ub){
-    return (n >= lb) && (n <= ub);
-}
 
 int main(){
     unsigned int sum{};
     char c;
     while(sum < 350){
         std::cin >> c;
-        auto n = static_cast<int>(c);
-        if(!in_range(n, 48, 57) && !in_range(n, 65, 90) && !in_range(n, 97, 122)) continue;
-        sum += n;
+        if(!std::isalnum(c)) continue;
+        sum += c;
         std::cout << sum << "\n";
     }
     return 0;
